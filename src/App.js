5b82619acd2813012e7c5b8b241a6c8040152750
drizzer <med_attia@hotmail.fr>
2023-01-09 23:12:26 +0100

    restored Katharina branch changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,83 @@
-function sayHello() {
-    alert('Hello');
-}
 
 function openNav() {
-    document.getElementById('mySidebar').style.width = '250px';
-    document.getElementById('main').style.marginLeft = '250px';
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
-    document.getElementById('mySidebar').style.width = '0';
-    document.getElementById('main').style.marginLeft = '0';
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
 }
-
-const loginDialog = document.getElementById('loginDialog');
+const loginDialog = document.getElementById("loginDialog");
 
 function openDialog() {
     loginDialog.showModal();
 }
 
 function acceptLogin() {
-    var usernameHTML = document.getElementById('username');
+    var usernameHTML = document.getElementById("username");
     var username = usernameHTML.value;
     console.log(username);
     loginDialog.close();
 
-    var loginElement = document.getElementById('loginElement');
-    var loginName = document.getElementById('loginName');
-    var loginLogo = document.getElementById('userLogo');
+    var loginElement = document.getElementById("loginElement");
+    var loginName = document.getElementById("loginName");
+    var loginLogo = document.getElementById("userLogo");
 
     loginName.innerText = username;
 
-    loginElement.style.display = 'none';
-    loginName.style.display = 'flex';
-    loginLogo.style.display = 'flex';
+    loginElement.style.display = "none";
+    loginName.style.display = "flex";
+    loginLogo.style.display = "flex";
 }
 
-//loginDialog.addEventListener('click',)
+
+//var womansection = getElementById("womansection");
+//var mansection = getElementById("mansection");
+//var seasonsection = getElementById("seasonsection");
+
+function toggle(elementId) {
+    console.log("here");
+    if (document.getElementById(elementId).style.display === "block") {
+        document.getElementById(elementId).style.display = "none"
+    } else {
+        document.getElementById(elementId).style.display = "block"
+    }
+}
+
+function toggleWomen() {
+    toggle("womansection");
+}
+
+function toggleMan() { toggle("mansection"); }
+function toggleSeason() { toggle("seasonsection"); }
+
+var prefix = "product";
+
+function hideProducts() {
+    Array(14).fill(1, 0, 13).forEach((elementId, index) => {
+        document.getElementById(prefix + (index + 1)).style.visibility = 'collapse';
+    });
+}
+
+function displayItems(idlist) {
+    hideProducts()
+    idlist.forEach((index) => {
+        document.getElementById(prefix + index).style.visibility = 'visible';
+    });
+    closeNav();
+}
+
+function showWomanCap() { displayItems([1, 2, 3]); }
+
+function showWomanCoat() { displayItems([4, 5]); }
+
+function showWomanPullover() { displayItems([8, 9]); }
+
+function showWomanGloves() { displayItems([6, 7]); }
+
+function showWomanShoes() { displayItems([10]); }
+
+function showWomanTrousers() { displayItems([11, 12, 13]); }
+
+function showAll() { displayItems(Array(14).fill(1, 0, 13).map((el, index) => { return index + 1; })); }
