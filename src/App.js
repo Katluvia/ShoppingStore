779e918167508657a31f9dfafbf918db854f86fd
drizzer <med_attia@hotmail.fr>
2023-01-09 23:02:15 +0100

    moved script from index.html to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,38 @@
 function sayHello() {
     alert('Hello');
 }
+
+function openNav() {
+    document.getElementById('mySidebar').style.width = '250px';
+    document.getElementById('main').style.marginLeft = '250px';
+}
+
+function closeNav() {
+    document.getElementById('mySidebar').style.width = '0';
+    document.getElementById('main').style.marginLeft = '0';
+}
+
+const loginDialog = document.getElementById('loginDialog');
+
+function openDialog() {
+    loginDialog.showModal();
+}
+
+function acceptLogin() {
+    var usernameHTML = document.getElementById('username');
+    var username = usernameHTML.value;
+    console.log(username);
+    loginDialog.close();
+
+    var loginElement = document.getElementById('loginElement');
+    var loginName = document.getElementById('loginName');
+    var loginLogo = document.getElementById('userLogo');
+
+    loginName.innerText = username;
+
+    loginElement.style.display = 'none';
+    loginName.style.display = 'flex';
+    loginLogo.style.display = 'flex';
+}
+
+//loginDialog.addEventListener('click',)
