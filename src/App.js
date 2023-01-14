d9d921e007be46ba2a50cd6386a544540d7abd63
Katluvia <katixhorses@googlemail.com>
2023-01-14 20:17:04 +0100

    added breadcrumbs, fixed issue 13,14,18,

--- src/App.js
+++ src/App.js
@@ -26,12 +26,12 @@ window.onclick = function (event) {
 
 function openNav() {
     document.getElementById('mySidebar').style.width = '250px';
-    document.getElementById('main').style.marginLeft = '250px';
+    //document.getElementById('main').style.marginLeft = '250px';
 }
 
 function closeNav() {
     document.getElementById('mySidebar').style.width = '0';
-    document.getElementById('main').style.marginLeft = '0';
+    //document.getElementById('main').style.marginLeft = '0';
 }
 const loginDialog = document.getElementById('loginDialog');
 
@@ -39,22 +39,61 @@ function openDialog() {
     loginDialog.showModal();
 }
 
+var loginElement = document.getElementById('loginElement');
+var loginName = document.getElementById('loginName');
+var loginLogo = document.getElementById('userLogo');
+var userFlexBox = document.getElementById("userFlexBox");
 function acceptLogin() {
     var usernameHTML = document.getElementById('username');
     var username = usernameHTML.value;
+    var password = document.getElementById("password").value;
     console.log(username);
-    loginDialog.close();
+    console.log(password);
 
-    var loginElement = document.getElementById('loginElement');
-    var loginName = document.getElementById('loginName');
-    var loginLogo = document.getElementById('userLogo');
+    if(username.length == 0 || password.length == 0){
+        document.getElementById("warningLogin").style.display = "flex";
+    }
+    else{
+        document.getElementById("warningLogin").style.display = "none";
+        loginDialog.close();
+        
 
-    loginName.innerText = username;
+        loginName.innerText = username;
 
-    loginElement.style.display = 'none';
-    loginName.style.display = 'flex';
-    loginLogo.style.display = 'flex';
+        loginElement.style.display = 'none';
+        loginName.style.display = 'flex';
+        loginLogo.style.display = 'flex';
+        userFlexBox.style.display = "flex";
+    }
 }
+
+var userContext = document.getElementById("userContext");
+var userFlexBox = document.getElementById("userFlexBox");
+
+function enterUserBox(){
+    document.getElementById("loginName").style.color = "black"
+    userContext.style.visibility = "visible";
+    var rect = userFlexBox.getBoundingClientRect();
+    console.log(rect["y"]);
+    console.log(rect);
+    userContext.style.left = rect["left"] + "px";
+    userContext.style.top = (rect["y"] + rect["height"])+ "px";
+}
+
+function leaveUserBox(){
+    document.getElementById("loginName").style.color = "white"
+    userContext.style.visibility = "collapse";
+}
+
+function userLogout(){
+    document.getElementById('username').value = "";
+    document.getElementById("password").value = "";
+    loginElement.style.display = 'flex';
+    loginName.style.display = 'none';
+    loginLogo.style.display = 'none';
+    userFlexBox.style.display = "none";
+}
+
 
 //var womansection = getElementById("womansection");
 //var mansection = getElementById("mansection");
@@ -86,8 +125,8 @@ function hideProducts() {
     Array(14)
         .fill(1, 0, 13)
         .forEach((elementId, index) => {
-            document.getElementById(prefix + (index + 1)).style.visibility =
-                'collapse';
+            document.getElementById(prefix + (index + 1)).style.visibility = "collapse";
+            console.log(prefix + (index + 1));
         });
 }
 
@@ -99,28 +138,58 @@ function displayItems(idlist) {
     closeNav();
 }
 
+var breadSlash1 = document.getElementById("breadSlash1");
+var breadcrumb1 = document.getElementById("breadcrumb1");
+var breadSlash2 = document.getElementById("breadSlash2");
+var breadcrumb2 = document.getElementById("breadcrumb2");
+
+function showBreadcrumbs(){
+    breadSlash1.style.visibility = "visible";
+    breadcrumb1.style.visibility = "visible";
+    breadSlash2.style.visibility = "visible";
+    breadcrumb2.style.visibility = "visible";
+}
+
 function showWomanCap() {
     displayItems([1, 2, 3]);
+    showBreadcrumbs();
+    breadcrumb1.innerText = "Woman";
+    breadcrumb2.innerText = "Cap";
 }
 
 function showWomanCoat() {
-    displayItems([4, 5]);
+    displayItems([4,5]);
+    showBreadcrumbs();
+    breadcrumb1.innerText = "Woman";
+    breadcrumb2.innerText = "Coat";
 }
 
 function showWomanPullover() {
     displayItems([8, 9]);
+    showBreadcrumbs();
+    breadcrumb1.innerText = "Woman";
+    breadcrumb2.innerText = "Pullover";
 }
 
 function showWomanGloves() {
     displayItems([6, 7]);
+    showBreadcrumbs();
+    breadcrumb1.innerText = "Woman";
+    breadcrumb2.innerText = "Gloves";
 }
 
 function showWomanShoes() {
     displayItems([10]);
+    showBreadcrumbs();
+    breadcrumb1.innerText = "Woman";
+    breadcrumb2.innerText = "Shoes";
 }
 
 function showWomanTrousers() {
     displayItems([11, 12, 13]);
+    showBreadcrumbs();
+    breadcrumb1.innerText = "Woman";
+    breadcrumb2.innerText = "Trousers";
 }
 
 function showAll() {
@@ -131,6 +200,10 @@ function showAll() {
                 return index + 1;
             })
     );
+    breadSlash1.style.visibility = "collapse";
+    breadcrumb1.style.visibility = "collapse";
+    breadSlash2.style.visibility = "collapse";
+    breadcrumb2.style.visibility = "collapse";
 }
 
 var product_modal = document.getElementById('product1');
